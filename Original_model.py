fad30729dd9738387f91b8581d193189001d6485
Vishvajeet-MT19AIE331 <141996908+Vishvajeet-MT19AIE331@users.noreply.github.com>
2024-04-03 23:00:20 +0530

    Revert "Modified model"

--- Original_model.py
+++ Original_model.py
@@ -12,9 +12,9 @@ X, y = iris.data, iris.target
 print(X.shape)
 # Define model parameters
 input_size = X.shape[1]
-hidden_size = 100
+hidden_size = 50
 num_classes = len(set(y))
-learning_rate = 0.0001
+learning_rate = 0.1
 batch_size = 32
 num_epochs = 10
 
